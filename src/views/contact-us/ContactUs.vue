737700948ac089dfbceda462f910ba50a3e4bb18
Sara Vey <veysara584@gmail.com>
2023-09-16 21:39:58 +0700

    improve contact us

--- src/views/contact-us/ContactUs.vue
+++ src/views/contact-us/ContactUs.vue
@@ -2,33 +2,23 @@
     <div class="contact-us-page">
         <section class="contact_us">
             <div class="container">
-                <div class="row">
-                    <h1 class="title text-center">Welcome to our School</h1>
-                    <div class="col-md-10 offset-md-1">
-                        <div class="contact_inner">
-                            <div class="row">
-                                <div class="col-md-10">
-                                    <div class="contact_form_inner">
-                                        <div class="contact_field">
-                                            <h3>Contatc Us</h3>
-                                            <p>Feel Free to contact us any time. We will get back to you as soon as we can!.</p>
-                                            <input type="text" class="form-control form-group" placeholder="Name" />
-                                            <input type="text" class="form-control form-group" placeholder="Email" />
-                                            <textarea class="form-control form-group" placeholder="Message"></textarea>
-                                            <button class="contact_form_submit">Send</button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-md-2">
-                                    <div class="right_conatct_social_icon d-flex align-items-end">
-                                    <div class="socil_item_inner d-flex">
-                                        <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-                                    </div>
-                                    </div>
+                <h1 class="title text-center">Welcome to our School</h1>
+                <div class="contact_inner">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="contact_form_inner">
+                                <div class="contact_field">
+                                    <h3>Contatc Us</h3>
+                                    <p>Feel Free to contact us any time. We will get back to you as soon as we can!.</p>
+                                    <input type="text" class="form-control form-group" placeholder="Name" />
+                                    <input type="text" class="form-control form-group" placeholder="Email" />
+                                    <textarea class="form-control form-group" placeholder="Message"></textarea>
+                                    <button class="contact_form_submit">Send</button>
                                 </div>
                             </div>
+                        </div>
+
+                        <div class="col-md-6">
                             <div class="contact_info_sec">
                                 <h4>Contact Info</h4>
                                 <div class="d-flex info_single align-items-center">
@@ -53,7 +43,7 @@
         <section class="map_sec">
             <div class="container">
                 <div class="row">
-                    <div class="col-md-10 offset-md-1">
+                    <div class="col-md-12">
                         <div class="map_inner">
                             <h4>Find Us on Google Map</h4>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quo beatae quasi assumenda, expedita aliquam minima tenetur maiores neque incidunt repellat aut voluptas hic dolorem sequi ab porro, quia error.</p>
